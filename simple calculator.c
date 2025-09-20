1a16e259c21ac87a8b3ec90251b70a6f1c48a39d
chandanashivap445-collab <chandanasp445@gmail.com>
2025-09-20 11:56:52 +0530

    Create simple calculator.c

---
+++ simple calculator.c
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double num1, num2, result;
+    int choice;
+
+    cout << "Simple Calculator\n";
+    cout << "-----------------\n";
+
+    // Input two numbers
+    cout << "Enter first number: ";
+    cin >> num1;
+    cout << "Enter second number: ";
+    cin >> num2;
+
+   
+    cout << "\nSelect operation:\n";
+    cout << "1. Addition (+)\n";
+    cout << "2. Subtraction (-)\n";
+    cout << "3. Multiplication (*)\n";
+    cout << "4. Division (/)\n";
+    cout << "Enter your choice (1-4): ";
+    cin >> choice;
+
+    
+    switch (choice) {
+        case 1:
+            result = num1 + num2;
+            cout << "Result = " << result << endl;
+            break;
+        case 2:
+            result = num1 - num2;
+            cout << "Result = " << result << endl;
+            break;
+        case 3:
+            result = num1 * num2;
+            cout << "Result = " << result << endl;
+            break;
+        case 4:
+            if (num2 != 0)
+                result = num1 / num2;
+            else {
+                cout << "Error! Division by zero is not allowed." << endl;
+                return 0;
+            }
+            cout << "Result = " << result << endl;
+            break;
+        default:
+            cout << "Invalid choice! Please run again." << endl;
+    }
+
+    return 0;
+}
